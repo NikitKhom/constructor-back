bbe4158c08ad0c596789516a028df084f09476dc
NikitKhom <nikita.khomenko.n25@gmail.com>
2025-02-24 12:22:35 +0300

    update userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,22 +5,26 @@ interface IUser {
     id: number;
     name: string;
     email: string;
+    avatar: string | null;
   }
 
 const getUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const userId = Number(req.params.id);
-    const { rows }: IUser[] = await pool.query<IUser>("SELECT * FROM users WHERE id = $1", [userId]);
-    // if (user.length === 0) {
-    //     res.status(404).json({ message: "Пользователь не найден" });
-    //     return;
-    // }
-    console.log(user);
-    res.status(200).json(user.rows);
+    const userId = req.params.id;
+    const result = await pool.query<IUser>("SELECT id, username, email, avatar FROM users WHERE id = $1", [userId]);
+    const user: IUser[] = result.rows;
+    if (user.length === 0) {
+      res.status(404).json({ message: "Пользователь не найден" });
+      return;
+    }
+    user[0].avatar = user[0].avatar ?? "";
+    res.status(200).json(user[0]);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
 };
+
+
 
 
 
